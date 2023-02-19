67c7b99ef23506890fecfacefa2b9e4bbdd3a589
gutosie <krzysztofgutosie@gmail.com>
2023-02-19 17:08:17 +0200

    Update getinfo.py

--- MBvu/getinfo.py
+++ MBvu/getinfo.py
@@ -10,7 +10,6 @@ dirMB='/usr/lib/enigma2/python/Plugins/Extensions/MBvu'
 dirscripts='/usr/lib/enigma2/python/Plugins/Extensions/MBvu/script'
 dirIscripts='/usr/lib/enigma2/python/Plugins/Extensions/MBvu/iscript'
 namber='4' or '5' or '6' or '7' or '8' or '9' or '10' or '11' or '12' or '13' or '14' or '15' or '16' or '17' or '18' or '19' or '20' #itd.. add
-namberto='4' or '5' or '6' or '7' or '8' or '9' 
 namberout = '10' or '11' or '12' or '13' or '14' or '15' or '16' or '17' or '18' or '19' or '20' 
 
 def getROOT():
